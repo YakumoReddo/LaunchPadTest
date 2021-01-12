1c651e8baa1bbd675e8e2089358c1a3f41e44a1a
YakumoReddo <3166458362@qq.com>
2021-01-12 21:51:27 +0800

    添加项目文件。

---
+++ launchpadTest2/Interface.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Midi;
+#pragma warning disable IDE1006 // 命名样式
+namespace LP
+{
+    public class Interface
+    {
+        private Pitch[,] notes = new Pitch[8, 8] {
+            { Pitch.A5, Pitch.ASharp5, Pitch.B5, Pitch.C6, Pitch.CSharp6, Pitch.D6, Pitch.DSharp6, Pitch.E6 },
+            { Pitch.B4, Pitch.C5, Pitch.CSharp5, Pitch.D5, Pitch.DSharp5, Pitch.E5, Pitch.F5, Pitch.FSharp5 },
+            { Pitch.CSharp4, Pitch.D4, Pitch.DSharp4, Pitch.E4, Pitch.F4, Pitch.FSharp4, Pitch.G4, Pitch.GSharp4 },
+            { Pitch.DSharp3, Pitch.E3, Pitch.F3, Pitch.FSharp3, Pitch.G3, Pitch.GSharp3, Pitch.A3, Pitch.ASharp3 },
+            { Pitch.F2, Pitch.FSharp2, Pitch.G2, Pitch.GSharp2, Pitch.A2, Pitch.ASharp2, Pitch.B2, Pitch.C3 },
+            { Pitch.G1, Pitch.GSharp1, Pitch.A1, Pitch.ASharp1, Pitch.B1, Pitch.C2, Pitch.CSharp2, Pitch.D2 },
+            { Pitch.A0, Pitch.ASharp0, Pitch.B0, Pitch.C1, Pitch.CSharp1, Pitch.D1, Pitch.DSharp1, Pitch.E1 },
+            { Pitch.BNeg1, Pitch.C0, Pitch.CSharp0, Pitch.D0, Pitch.DSharp0, Pitch.E0, Pitch.F0, Pitch.FSharp0 }
+        };
+
+        private Pitch[] rightLEDnotes = new Pitch[] {
+            Pitch.F6, Pitch.G5, Pitch.A4, Pitch.B3, Pitch.CSharp3, Pitch.DSharp2, Pitch.F1, Pitch.G0
+        };
+        public enum ControlButton
+        {
+            UP,DOWN,LEFT,RIGHT,SESSION,USER1,USER2,MIXER,F0,F1,F2,F3,F4,F5,F6,F7,NONE
+        }
+
+        public InputDevice targetInput;
+        public OutputDevice targetOutput;
+
+        public delegate void LaunchpadKeyEventHandler(object source, LaunchpadKeyEventArgs e);
+
+        public delegate void LaunchpadCCKeyEventHandler(object source, LaunchpadCCKeyEventArgs e);
+
+        public event LaunchpadKeyEventHandler OnLaunchpadKeyPressed;
+        public event LaunchpadCCKeyEventHandler OnLaunchpadCCKeyPressed;
+
+        public class LaunchpadCCKeyEventArgs : EventArgs
+        {
+            private ControlButton val;
+            private bool press;
+            public LaunchpadCCKeyEventArgs(int _val,int _vol)
+            {
+                switch (_val)
+                {
+                    case 104:
+                        val = ControlButton.UP;
+                        break;
+                    case 105:
+                        val = ControlButton.DOWN;
+                        break;
+                    case 106:
+                        val = ControlButton.LEFT;
+                        break;
+                    case 107:
+                        val = ControlButton.RIGHT;
+                        break;
+                    case 108:
+                        val = ControlButton.SESSION;
+                        break;
+                    case 109:
+                        val = ControlButton.USER1;
+                        break;
+                    case 110:
+                        val = ControlButton.USER2;
+                        break;
+                    case 111:
+                        val = ControlButton.MIXER;
+                        break;
+                    case 89:
+                        val = ControlButton.F0;
+                        break;
+                    case 79:
+                        val = ControlButton.F1;
+                        break;
+                    case 69:
+                        val = ControlButton.F2;
+                        break;
+                    case 59:
+                        val = ControlButton.F3;
+                        break;
+                    case 49:
+                        val = ControlButton.F4;
+                        break;
+                    case 39:
+                        val = ControlButton.F5;
+                        break;
+                    case 29:
+                        val = ControlButton.F6;
+                        break;
+                    case 19:
+                        val = ControlButton.F7;
+                        break;
+                    default:
+                        val = ControlButton.NONE;
+                        break;
+                }
+                if (_vol == 127) press = true;
+                else press = false;
+            }
+            public ControlButton GetVal()
+            {
+                return val;
+            }
+            public bool isPressing()
+            {
+                return press;
+            }
+        }
+
+        public class LaunchpadKeyEventArgs : EventArgs
+        {
+            private int x;
+            private int y;
+            private bool press;
+            private Midi.NoteOnMessage msg;
+            public LaunchpadKeyEventArgs(int _pX, int _pY,int _vol)
+            {
+                x = _pX;
+                y = _pY;
+                msg = null;
+                if (_vol == 127) press = true;
+                else press = false;
+            }
+            public LaunchpadKeyEventArgs(Midi.NoteOnMessage _msg,int _pX, int _pY,int _vol)
+            {
+                msg = _msg;
+                x = _pX;
+                y = _pY;
+                if (_vol == 127) press = true;
+                else press = false;
+            }
+            public int GetX()
+            {
+                return x;
+            }
+            public int GetY()
+            {
+                return y;
+            }
+            public Midi.NoteOnMessage getMSG()
+            {
+                return msg;
+            }
+            public bool isPressing()
+            {
+                return press;
+            }
+        }
+
+
+        private void midiPress(Midi.NoteOnMessage msg)
+        {
+            if (OnLaunchpadKeyPressed != null && !rightLEDnotes.Contains(msg.Pitch))
+            {
+                OnLaunchpadKeyPressed(this, new LaunchpadKeyEventArgs(msg,midiNoteToLed(msg.Pitch)[0], midiNoteToLed(msg.Pitch)[1],msg.Velocity));
+            }
+            else if (OnLaunchpadCCKeyPressed != null && rightLEDnotes.Contains(msg.Pitch))
+            {
+                OnLaunchpadCCKeyPressed(this, new LaunchpadCCKeyEventArgs(midiNoteToSideLED(msg.Pitch), msg.Velocity));
+            }
+
+        }
+
+        public int midiNoteToSideLED(Pitch p)
+        {
+            switch (p)
+            {
+                case Pitch.F6:
+                    return 89;
+                case Pitch.G5:
+                    return 79;
+                case Pitch.A4:
+                    return 69;
+                case Pitch.B3:
+                    return 59;
+                case Pitch.CSharp3:
+                    return 49;
+                case Pitch.DSharp2:
+                    return 39;
+                case Pitch.F1:
+                    return 29;
+                case Pitch.G0:
+                    return 19;
+                default:
+                    return 0;
+            }
+        }
+
+        public int[] midiNoteToLed(Pitch p)
+        {
+            for (int x = 0; x <= 7; x++)
+            {
+                for (int y = 0; y <= 7; y++)
+                {
+                    if (notes[x, y] == p)
+                    {
+                        int[] r1 = { x, y };
+                        return r1;
+                    }
+                }
+            }
+            int[] r2 = { 0, 0 };
+            return r2;
+        }
+
+        public Pitch ledToMidiNote(int x, int y)
+        {
+            return notes[x, y];
+        }
+
+        public void clearAllLEDs()
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    setLED(x, y, 0);
+                }
+            }
+
+            for (int ry = 0; ry < 8; ry++)
+            {
+                setSideLED(ry, 0);
+            }
+
+            for (int tx = 1; tx < 9; tx++)
+            {
+                setTopLEDs(tx, 0);
+            }
+        }
+
+        public void setTopLEDs(int x, int velo)
+        {
+            byte[] data = { 240, 0, 32, 41, 2, 24, 10, Convert.ToByte(103 + x), Convert.ToByte(velo), 247 };
+            targetOutput.SendSysEx(data);
+        }
+
+        public void setSideLED(int y, int velo)
+        {
+            targetOutput.SendNoteOn(Channel.Channel1, rightLEDnotes[y], velo);
+        }
+        public void setLED(int x, int y, int velo)
+        {
+            try
+            {
+                targetOutput.SendNoteOn(Channel.Channel1, notes[x, y], velo);
+            }
+            catch (Midi.DeviceException)
+            {
+                Console.WriteLine("<< LAUNCHPAD.NET >> Midi.DeviceException");
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Returns all connected and installed Launchpads.
+        /// </summary>
+        /// <returns>Returns LaunchpadDevice array.</returns>
+        public LaunchpadDevice[] getConnectedLaunchpads()
+        {
+            List<LaunchpadDevice> tempDevices = new List<LaunchpadDevice>();
+
+            foreach (InputDevice id in Midi.InputDevice.InstalledDevices)
+            {
+                foreach (OutputDevice od in Midi.OutputDevice.InstalledDevices)
+                {
+                    if (id.Name == od.Name)
+                    {
+                        if (id.Name.ToLower().Contains("launchpad"))
+                        {
+                            tempDevices.Add(new LaunchpadDevice(id.Name));
+                        }
+                    }
+                }
+            }
+
+            return tempDevices.ToArray();
+        }
+
+        public void onControlChange(Midi.ControlChangeMessage e)
+        {
+            if (OnLaunchpadCCKeyPressed != null)
+            {
+                OnLaunchpadCCKeyPressed(this, new LaunchpadCCKeyEventArgs(int.Parse(e.Control.ToString()),e.Value));
+            }
+        }
+
+        public bool connect(LaunchpadDevice device)
+        {
+            foreach (InputDevice id in Midi.InputDevice.InstalledDevices)
+            {
+                if (id.Name.ToLower() == device._midiName.ToLower())
+                {
+                    targetInput = id;
+                    id.Open();
+                    targetInput.ControlChange += new InputDevice.ControlChangeHandler(onControlChange);
+                    targetInput.NoteOn += new InputDevice.NoteOnHandler(midiPress);
+                    targetInput.StartReceiving(null);
+                }
+            }
+            foreach (OutputDevice od in Midi.OutputDevice.InstalledDevices)
+            {
+                if (od.Name.ToLower() == device._midiName.ToLower())
+                {
+                    targetOutput = od;
+                    od.Open();
+                }
+            }
+
+            return true;
+        }
+
+        public bool disconnect(LaunchpadDevice device)
+        {
+            if (targetInput.IsOpen && targetOutput.IsOpen)
+            {
+                targetInput.StopReceiving();
+                targetInput.Close();
+                targetOutput.Close();
+            }
+            return !targetInput.IsOpen && !targetOutput.IsOpen;
+        }
+
+        public class LaunchpadDevice
+        {
+            public string _midiName;
+
+            public LaunchpadDevice(string name)
+            {
+                _midiName = name;
+            }
+        }
+    }
+}
+#pragma warning restore IDE1006 // 命名样式
---
+++ launchpadTest2/Program.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LP;
+
+namespace launchpadTest2
+{
+    class Program
+    {
+        private static Interface lp;
+        static void Main(string[] args)
+        {
+            lp = new Interface();
+
+            lp.connect(lp.getConnectedLaunchpads()[0]);
+
+            lp.OnLaunchpadKeyPressed += new Interface.LaunchpadKeyEventHandler(btnPressEvent);
+            lp.OnLaunchpadCCKeyPressed += new Interface.LaunchpadCCKeyEventHandler(ctrPressEvent);
+            Console.WriteLine("游玩贪吃蛇请输入tcs 退出请输入quit 清空光效请输入clear");
+
+            while (true)
+            {
+                string str = Console.ReadLine();
+                if (str.Equals("quit"))
+                {
+                    break;
+                }
+                else if (str.Equals("tcs"))
+                {
+                    clearLight();
+                    tcs t = new tcs(lp);
+                }
+                else if (str.StartsWith("test"))
+                {
+                    test(int.Parse(str.Substring(4)));
+                }
+                else if (str.Equals("clear"))
+                {
+                    clearLight();
+                }
+                else
+                {
+                    string[] strs = str.Split(' ');
+                    show(int.Parse(strs[0]), int.Parse(strs[1]), int.Parse(strs[2]));
+                }
+            }
+            clearLight();
+            lp.disconnect(lp.getConnectedLaunchpads()[0]);
+        }
+        private static void clearLight()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    show(j, i, 0);
+                }
+            }
+        }
+        private static void test(int start)
+        {
+            int cnt = start;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    show(j, i, cnt);
+                    cnt++;
+                }
+            }
+        }
+
+        public static void ctrPressEvent(object sender, Interface.LaunchpadCCKeyEventArgs e)
+        {
+            //Console.WriteLine("Control: " + e.GetVal().ToString() + " " + (e.isPressing() ? "in" : "out"));
+        }
+
+        public static void btnPressEvent(object sender, Interface.LaunchpadKeyEventArgs e)
+        {
+            //Console.WriteLine("X: " + e.GetY() + " ,Y: " + e.GetX() + " " + (e.isPressing() ? "in" : "out"));
+        }
+        private static void show(int x, int y, int vol)
+        {
+            lp.setLED(y, x, vol);
+        }
+    }
+    class tcs
+    {
+        class Pos
+        {
+            public int x;
+            public int y;
+            public Pos(int _x, int _y)
+            {
+                x = _x;
+                y = _y;
+            }
+            public Pos padd(int _x,int _y)
+            {
+                x += _x;
+                y += _y;
+                return this;
+            }
+            public Pos set(Pos p)
+            {
+                x = p.x;
+                y = p.y;
+                return this;
+            }
+            public Pos set(int _x,int _y)
+            {
+                x = _x;
+                y = _y;
+                return this;
+            }
+            public Pos clone()
+            {
+                return new Pos(x, y);
+            }
+            public override bool Equals(object obj)
+            {
+                if (obj == null) return false;
+                if (obj.GetType().Equals(this.GetType()) == false) return false;
+                Pos p = (Pos)obj;
+                if (p.x == this.x && p.y == this.y) return true;
+                return false;
+            }
+            public override int GetHashCode()
+            {
+                return x * 10 + y;
+            }
+        }
+        private int[,] dir = new int[8, 8]
+        {
+            { 0, 1, 1, 1, 1, 1, 1, 0 },
+            { 2, 0, 1, 1, 1, 1, 0, 3 },
+            { 2, 2, 0, 1, 1, 0, 3, 3 },
+            { 2, 2, 2, 0, 0, 3, 3, 3 },
+            { 2, 2, 2, 0, 0, 3, 3, 3 },
+            { 2, 2, 0, 4, 4, 0, 3, 3 },
+            { 2, 0, 4, 4, 4, 4, 0, 3 },
+            { 0, 4, 4, 4, 4, 4, 4, 0 }
+        };
+        private enum Direction
+        {
+            UP, DOWN, LEFT, RIGHT, NONE
+        }
+        private Direction getDirection(Pos pos)
+        {
+            switch (dir[pos.x, pos.y])
+            {
+                case 1:
+                    return Direction.UP;
+                case 2:
+                    return Direction.LEFT;
+                case 3:
+                    return Direction.RIGHT;
+                case 4:
+                    return Direction.DOWN;
+                default:
+                    return Direction.UP;
+            }
+        }
+
+        private Interface lp;
+
+        private void show(int x,int y,int vol)
+        {
+            lp.setLED(y, x, vol);
+        }
+        private void show(Pos pos,int vol)
+        {
+            lp.setLED(pos.y, pos.x, vol);
+        }
+        private Random rd;
+        private Pos scr;
+        private Direction Sdir;
+        private Direction Ldir;
+        private Pos head;
+        private Pos getRandom()
+        {
+            return new Pos(rd.Next(0, 8), rd.Next(0, 8));
+        }
+        private void init()
+        {
+            loc = false;
+            show(3, 4, 3);
+            show(3, 3, 3);
+            q = new Queue<Pos>();
+            q.Enqueue(new Pos(3, 4));
+            q.Enqueue(new Pos(3, 3));
+            rd = new Random();
+            scr = getRandom();
+            while (q.Contains(scr))
+            {
+                scr.set(getRandom());
+            }
+            show(scr, 19);
+            Sdir = Direction.UP;
+            Ldir = Direction.UP;
+            head = new Pos(3, 3);
+
+        }
+        private bool loc;
+        private Queue<Pos> q;
+        private int score = 0;
+        private Pos getNear(Pos pos,Direction _d)
+        {
+            //Console.WriteLine("getNextOf (" + pos.x + "," + pos.y + ")");
+            switch (_d)
+            {
+                case Direction.UP:
+                    return pos.padd(0, -1);
+                case Direction.DOWN:
+                    return pos.padd(0, 1);
+                case Direction.LEFT:
+                    return pos.padd(-1, 0);
+                case Direction.RIGHT:
+                    return pos.padd(1, 0);
+                default:
+                    return pos.padd(0, -1);
+            }
+        }
+        public void onChangeDir(object sender,Interface.LaunchpadKeyEventArgs e)
+        {
+            if (loc) return;
+            Pos pos = new Pos(e.GetX(), e.GetY());
+            if ((Ldir == Direction.UP && getDirection(pos) == Direction.DOWN) || (Ldir == Direction.DOWN && getDirection(pos) == Direction.UP) || (Ldir == Direction.LEFT && getDirection(pos) == Direction.RIGHT) || (Ldir == Direction.RIGHT && getDirection(pos) == Direction.LEFT)) return;
+            Sdir = getDirection(pos);
+        }
+        private void run()
+        {
+            int dur = 1000;
+            int startTime = Environment.TickCount;
+            while (true)
+            {
+                if (Math.Abs(Environment.TickCount - startTime) >= dur)
+                {
+                    loc = true;
+                    Pos next = getNear(head.clone(), Sdir);
+                    Ldir = Sdir;
+                    //Console.WriteLine("(" + head.x + "," + head.y + ") (" + next.x + "," + next.y + ") (" + scr.x + "," + scr.y + ")");
+                    if (q.Contains(next)) return;
+                    if (next.x < 0 || next.x > 7 || next.y < 0 || next.y > 7) return;
+                    if (!next.Equals(scr))
+                    {
+                        show(q.Dequeue(), 0);
+                    }
+                    else
+                    {
+                        score++;
+                        dur -= 20;
+
+                        scr.set(getRandom());
+                        while (q.Contains(scr))
+                        {
+                            scr.set(getRandom());
+                        }
+                        show(scr, 19);
+                    }
+                    show(next, 3);
+                    q.Enqueue(next);
+                    head.set(next);
+
+                    startTime = Environment.TickCount;
+                    loc = false;
+                }
+            }
+        }
+
+
+
+        public tcs(Interface _lp)
+        {
+            lp = _lp;
+            
+            init();
+            lp.OnLaunchpadKeyPressed += new Interface.LaunchpadKeyEventHandler(onChangeDir);
+            run();
+            while (q.Count > 0)
+            {
+                show(q.Dequeue(), 1);
+            }
+            Console.WriteLine("score: " + score);
+            Console.WriteLine("再次游玩请输入\"tcs\"");
+        }
+    }
+}
